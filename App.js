46deeb0dab7592009cfe9c3d25d7e2b590fa07ba
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 19:01:03 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Comforter: require('./assets/fonts/Comforter-Regular.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
+      <Text style={styles.heading}>My heading</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +27,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontFamily: 'Comforter',
+    fontSize: 20,
+  }
 });
